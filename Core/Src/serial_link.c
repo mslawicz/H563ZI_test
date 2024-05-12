295c2aec27701e61e2915fe9434ba591c794c08b
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-05-12 23:15:01 +0200

    DMA reception with 3 interrupts; to be corrected

--- Core/Src/serial_link.c
+++ Core/Src/serial_link.c
@@ -11,7 +11,7 @@ UART_HandleTypeDef* pSerialLinkUart = NULL;
 void serialLinkHandler(void)
 {
     uint8_t counter = 0;
-    HAL_UART_Receive_DMA(pSerialLinkUart, rxBuf, 23);
+    HAL_UARTEx_ReceiveToIdle_DMA(pSerialLinkUart, rxBuf, 15);
     /* Infinite loop */
     for(;;)
     {
@@ -34,7 +34,36 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     if(huart == pSerialLinkUart)
     {
         HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST1_Pin, GPIO_PIN_SET);
-        HAL_UART_Receive_DMA(pSerialLinkUart, rxBuf, 10);
+        HAL_UARTEx_ReceiveToIdle_DMA(pSerialLinkUart, rxBuf, 15);
         HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST1_Pin, GPIO_PIN_RESET);
+    }
+}
+
+/**
+  * @brief  Reception Event Callback (Rx event notification called after use of advanced reception service).
+  * @param  huart UART handle
+  * @param  Size  Number of data available in application reception buffer (indicates a position in
+  *               reception buffer until which, data are available)
+  * @retval None
+  */
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+    if(huart == pSerialLinkUart)
+    {
+        HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST2_Pin, GPIO_PIN_SET);
+        HAL_UARTEx_ReceiveToIdle_DMA(pSerialLinkUart, rxBuf, 15);
+        HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST2_Pin, GPIO_PIN_RESET);
+        if(Size == 7)
+        {
+            HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST2_Pin, GPIO_PIN_RESET);
+        }
+        else if(Size > 7)
+        {
+            HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST2_Pin, GPIO_PIN_RESET);
+        }
+        else
+        {
+            HAL_GPIO_WritePin(TEST1_GPIO_Port, TEST2_Pin, GPIO_PIN_RESET);
+        }
     }
 }
